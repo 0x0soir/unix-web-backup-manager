9f02b7a2fde19d8ab83ef695851c69868c354a3c
Jorge Parrilla <isoir.21@gmail.com>
2018-05-01 18:56:29 +0200

    Ignore secrets file

--- secrets.php
+++
@@ -1,6 +0,0 @@
-<?php
-defined('BASE_PATH') OR exit('No direct script access allowed');
-
-define('SMTP_HOST', 'smtp.gmail.com');
-define('SMTP_FROM', 'jparrl00@estudiantes.unileon.es');
-define('SMTP_PASSWORD', 'changeme');
