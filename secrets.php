7d81731e54d663247c187cae873f29590956ab3d
Jorge Parrilla <isoir.21@gmail.com>
2018-06-12 18:43:38 +0200

    Secrets file with MySQL config

--- secrets.php
+++ secrets.php
@@ -1,6 +1,13 @@
 <?php
 defined('BASE_PATH') OR exit('No direct script access allowed');
 
+# SMTP Connection
 define('SMTP_HOST', 'smtp.gmail.com');
 define('SMTP_FROM', 'jparrl00@estudiantes.unileon.es');
 define('SMTP_PASSWORD', 'changeme');
+
+# MySQL Connection
+define('MYSQL_HOST', 'localhost');
+define('MYSQL_USER', 'root');
+define('MYSQL_PASSWORD', 'changeme');
+define('MYSQL_DATABASE', 'tfg_admin');
