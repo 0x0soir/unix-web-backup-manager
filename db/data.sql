f66521d27124598b61ff8510ecf23db5ebe397de
Jorge Parrilla <isoir.21@gmail.com>
2018-06-13 17:32:27 +0200

    Default admin user with correct data.

--- db/data.sql
+++ db/data.sql
@@ -1,7 +1,7 @@
 /* Password: rasmuslerdorf */
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `session`)
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `session`, `last_login`, `active`, `max_size`, `used_size`, `send_memory_mails`, `send_backup_done_mails`, `root_path`, `rgpd_status`)
 VALUES
-	(1, 'admin', 'admin@tfg.com', '$2y$12$3OahiXpTqjwSI4wJY4sT1OB1han4FDiPKe6WHBnQYXJ4k6VF0KQqy', '');
+	(1, 'admin', 'admin@tfg.com', '$2y$12$3OahiXpTqjwSI4wJY4sT1OB1han4FDiPKe6WHBnQYXJ4k6VF0KQqy', '', NULL, 1, 500000, 0, 1, 1, NULL, 0);
 
 INSERT INTO `user_perms` (`id`, `name`, `definition`)
 VALUES
